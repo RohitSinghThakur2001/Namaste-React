25375cdc3f3f1ef6a932681aed87307152e1079f
Rohit Singh Thakur <rohitsinghdamoh@gmail.com>
2026-04-22 15:18:21 +0530

    initial commit

---
+++ Index.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div",
+    {id:"Parent"},
+  
+    React.createElement("div",
+        {id:"Child1"},
+        [React.createElement("h1",{id:"three",key:"two"},"this is h1 tag"),
+        React.createElement("h2",{id:"four",key:"one"  },"this is h2 tag"),
+        React.createElement("h2",{id:"four",key:"THREE"  },"this is h2 tag"),
+    ]
+    )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
